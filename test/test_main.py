ff5d544f528c45f3940939fd7d9a38743921dc2d
Adriel2004 <adrielgonza7@gmail.com>
2026-07-25 21:45:28 -0600

    Primer commit - PROYECTO GIT PRACTICA

---
+++ test/test_main.py
@@ -0,0 +1,28 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
+
+
+def test_predict_valid_payload():
+    response = client.post(
+        "/predict",
+        json={"input": "becas"}
+    )
+    assert response.status_code == 200
+    assert "result" in response.json()
+
+
+def test_predict_valid_error():
+    response = client.post(
+        "/predict",
+        json={"input": "becas"}
+    )
+    assert response.status_code == 201
+    assert "result" in response.json()
